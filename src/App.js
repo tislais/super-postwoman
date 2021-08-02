ab8a9497b4d213a83b4ed67e42a451999754072e
Tis Lais <tistististis@gmail.com>
2021-08-01 21:33:26 -0700

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Main from './containers/Main';
 
 function App() {
   return (
-    <div className="main">
-      
+    <div>
+      <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header 
+      className="bg-gradient-to-r from-purple-500 via-pink-500 to-red-400 fixed w-full
+        h-12 flex items-center pl-4">
+      <h1 className="text-xl font-semibold text-white tracking-wide shadow-2xl">Super Postwoman</h1>
+    </header>
+  )
+};
+
+export default Header;
